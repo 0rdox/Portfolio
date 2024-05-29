b4a9f45cab44105f05e94a54b273c04fc299dacc
J.Seremak <janko2002@outlook.com>
2024-05-29 18:19:17 +0200

    Commit to pc

--- portfolio/src/App.tsx
+++ portfolio/src/App.tsx
@@ -1,10 +1,39 @@
+import { BrowserRouter } from "react-router-dom";
+
+import {
+  About,
+  Contact,
+  Experience,
+  Feedbacks,
+  Hero,
+  Navbar,
+  Tech,
+  Works,
+  StarsCanvas,
+} from "./components";
+
 const App = () => {
   return (
-    <div>
-      <h1>Portfolio</h1>
-      <p>My name is John Doe and I am a web developer.</p>
-    </div>
+    <BrowserRouter>
+      <div className="relative bg-primary z-0">
+        <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
+          <Navbar />
+          <Hero />
+        </div>
+      </div>
+    </BrowserRouter>
   );
 };
 
 export default App;
+
+// Portfolio Project:
+// Frontend:
+// React.js (react three fiber)     V
+// Tailwind                         V
+// 	+ daisyUI? / other
+// Three.js
+// Material UI (component based html)
+
+// Hosting:
+// https://vercel.com/
